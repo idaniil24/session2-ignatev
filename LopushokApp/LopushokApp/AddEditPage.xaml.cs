f4d18a313377e704fb5cb1e0fd60341ab45b5e19
idaniil24 <i_daniil24@mail.ru>
2021-12-15 16:21:00 +0600

    first commit

---
+++ LopushokApp/LopushokApp/AddEditPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LopushokApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Window
+    {
+        private products _currentProduct = new products();
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentProduct;
+            TypesCombobox.ItemsSource = user10Entities.GetContext().ProductsTypes.ToList();
+    }
+        private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+                if (_currentProduct.ProductID == null)
+            {
+                user10Entities.GetContext().products.Add(_currentProduct);
+            }
+
+            try
+                {
+                    user10Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }            
+        }
+    }
+}
---
+++ LopushokApp/LopushokApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LopushokApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var currentproducts = user10Entities.GetContext().products.ToList();            
+            InitializeComponent();
+            LWProducts.ItemsSource = currentproducts;
+            ComboTypes.ItemsSource = user10Entities.GetContext().ProductsTypes.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditPage addEditPage = new AddEditPage();
+            addEditPage.Show();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditPage addEditPage = new AddEditPage();
+            addEditPage.Show();
+        }
+    }
+}
